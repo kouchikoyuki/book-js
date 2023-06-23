e1f390612a91d94c9d38918d117be0ca791d0400
kouchikoyuki <131890474+kouchikoyuki@users.noreply.github.com>
2023-06-23 16:27:01 +0900

    0623

--- 3-09_fizzbuzz/fizzbuzz/main.js
+++ 3-09_fizzbuzz/fizzbuzz/main.js
@@ -2,8 +2,8 @@ console.log('test')
 
 function fizzbuzz (num){
     if(num % 3 === 0 && num % 5 === 0) {
-        return 'fizzbuzz';
-    } else if(num % 3 === 0) {
+        return 'FizzBuzz';
+    }else if(num % 3 === 0) {
         return 'fizz';
     } else if(num % 5 === 0) {
         return 'buzz';
@@ -12,4 +12,4 @@ function fizzbuzz (num){
     }
 }
 
-fizzbuzz(12);
+FizzBuzz(15);
